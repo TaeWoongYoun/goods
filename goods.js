84c2b7a2f3a13f70ad84224109aff8588efefd18
xodnd0507 <24457545yong@gmail.com>
2024-03-11 13:59:20 +0900

    test : Add product and change style

--- goods.js
+++ goods.js
@@ -1,5 +1,6 @@
 var data = [
-    {idx: 1, img: "goods/01.jpg", sale: 12000, title: "바지", price: 3500, group: "의류"}
+    {idx: 1, img: "goods/01.jpg", sale: 12000, title: "바지", price: "3,500", group: "의류"},
+    {idx: 2, img: "goods/02.jpg", sale: 17500, title: "티셔츠", price: "23,000",group: "의류"}
 ]
 
 data.forEach((a, i)=>{
@@ -12,8 +13,7 @@ data.forEach((a, i)=>{
         <p>할인 : ${data[i].sale}</p>
         <p>그룹 : ${data[i].group}</p>
         <button>가격 : ${data[i].price}</button>
-    </div>
-</div>`
+    </div>`
 
     $('.product').append(템플릿)
 })
